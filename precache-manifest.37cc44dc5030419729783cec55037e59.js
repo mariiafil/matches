c29566363f94f995c1622ae584b44845d853e779
masha <mariafil2107@gmail.com>
2020-03-21 19:52:27 +0200

    Updates

---
+++ precache-manifest.37cc44dc5030419729783cec55037e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caced288c3f93b1151714eaa2a2b59d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7495947b0f78fe348b76",
+    "url": "./static/css/main.57dd0ee3.chunk.css"
+  },
+  {
+    "revision": "c1f4ad8d7d764e27172d",
+    "url": "./static/js/2.96b4237d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.96b4237d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7495947b0f78fe348b76",
+    "url": "./static/js/main.4da318c2.chunk.js"
+  },
+  {
+    "revision": "1a7077c253aad7ad7cd9",
+    "url": "./static/js/runtime-main.251cf660.js"
+  },
+  {
+    "revision": "400c697f4ab4f776b621fb351b662500",
+    "url": "./static/media/active.400c697f.png"
+  }
+]);
